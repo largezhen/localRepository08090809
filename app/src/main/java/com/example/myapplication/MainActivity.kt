ee51b9c7847f7b6b347551fa3e47a1879275291f
largezhen <372425109@qq.com>
2021-08-09 14:59:01 +0800

    commit 2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d("test", "test")
+        Log.d("test", "test2")
     }
 }
